7f84aeacd14ae4beb2a4f8499cc910dcd39d3a04
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-12 02:46:27 +0100

    addCarForm

--- samochody.php
+++ samochody.php
@@ -7,14 +7,12 @@ if (isset($_POST['samochodySumbit'])) {
     if ($conn->connect_error) {
         die("Connection error: " . $connect_error);
     }
-
     $marka = $_POST['marka'];
     $model = $_POST['model'];
     $rocznik = $_POST['rocznik'];
     $przebieg = $_POST['przebieg'];
 
     $sql = "INSERT INTO `samochod` ( `id_klienta`, `marka`, `model`, `rocznik`, `przebieg`) VALUES ( '1', '$marka', '$model', '$rocznik', '$przebieg');";
-
 
     if ($conn->query($sql)) {
         echo "<div class='success' id='message'> Zmiany zostały zachowane pomyślnie! </div>";
